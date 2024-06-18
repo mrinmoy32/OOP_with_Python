5491c5ea92ba2da6ede3959ebfa844915abb8901
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-18 23:27:33 +0530

    oop chapter 2

---
+++ OOP_Principles/oop_in_python2.py
@@ -0,0 +1,5 @@
+# ------------------------multilvel inheritance----------------------------
+
+# Multilevel inheritance is when a class is derived from a class which is also derived from another class.
+# class A is parent class of class B and class B is parent class of class C
+# This is kind of like prototypal inheritance in JavaScript
